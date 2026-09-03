ac7a41977a86968bfb1a76a9793dfea3543374ef
Pc <cannel5251@gmail.com>
2026-09-04 00:04:40 +0700

    new

---
+++ mosh/Constant.java
@@ -0,0 +1,7 @@
+public class Constant {
+    public static void main(String[] args) {
+        final double PI = 3.14159; // constant variable, cannot be changed
+        //we use pi to calculate area of circle, circumference of circle, volume of sphere, etc
+        System.out.println(PI);
+    }
+}
--- mosh/multiArray.java
+++ mosh/multiArray.java
@@ -1,17 +1,28 @@
+import java.util.Arrays;
+
 public class multiArray {
     public static void main(String[] args) {
         int[][] myNumbers = new int[2][3];
-        myNumbers[0][0] = 1;
-        myNumbers[0][1] = 2;
-        myNumbers[0][2] = 3;
-        myNumbers[1][0] = 4;
-        myNumbers[1][1] = 5;
-        myNumbers[1][2] = 6;
+    // 10 20 0 
+    //  0  0 30
+//    0 0 10
+//    0 1 20
+//    1 2 30
 
-        for (int i = 0; i < myNumbers.length; i++) {
-            for (int j = 0; j < myNumbers[i].length; j++) {
-                System.out.println(myNumbers[i][j]);
-            }
-        }
+// int[][] myNumbers2 = { {10, 20, 0}, {0, 0, 30} };
+    
+        myNumbers[0][0] = 10;
+        myNumbers[0][1] = 20;
+        myNumbers[1][2] = 30;   
+        
+         System.out.println(Arrays.deepToString(myNumbers) ); 
+         // deepToString() method is used to print multi-dimensional array in a readable format
+
+        // for (int i = 0; i < myNumbers.length; i++) {
+        //     for (int j = 0; j < myNumbers[i].length; j++) {
+        //         System.out.println(myNumbers[i][j]);
+        //     }
+        // }
+
     }
 }
